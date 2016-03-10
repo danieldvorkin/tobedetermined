c9d70848a0dd2f519ef40918f7482eb2ffb2c5b7
Daniel Dvorkin <Dvorkin212@gmail.com>
2016-03-09 21:28:09 -0500

    Working on building out the admin portal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if current_admin
+      admin_dashboard_index_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
